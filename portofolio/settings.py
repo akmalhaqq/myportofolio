79d9f75695a5aa0221a52750ec14086406b319a3
akmalhaqq <akmalhaqqqani@gmail.com>
2026-09-02 14:13:53 +0700

    Menambahkan Middleware WhiteNoise, STATIC_ROOT, dan WHITENOISE_USE_FINDERS di settings.py

--- portofolio/settings.py
+++ portofolio/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -77,6 +78,8 @@ TEMPLATES = [
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 WSGI_APPLICATION = 'portofolio.wsgi.application'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+WHITENOISE_USE_FINDERS = True
 
 
 # Database
