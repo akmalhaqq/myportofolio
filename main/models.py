e2e4060d1c23eb404fcdfeefd715e05d5c7dbf52
akmalhaqq <akmalhaqqqani@gmail.com>
2026-09-09 10:47:53 +0700

    Memasukkan data asli untuk Experience dan membuat style CSS yang persis dengan bentuk asli di Tugas 1 bagian Experience

--- main/models.py
+++ main/models.py
@@ -4,15 +4,6 @@ from django.db import models
 import uuid
 from django.db import models
 class Experience(models.Model):
-    EXPERIENCE_CHOICES = [
-    ('internship', 'Internship'),
-    ('research', 'Research'),
-    ('volunteer', 'Volunteer'),
-    ('part-time', 'Part-Time'),
-    ('full-time', 'Full-Time'),
-    ('freelance', 'Freelance'),
-    ]
-
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     order = models.PositiveBigIntegerField(default=0)
     title = models.CharField(max_length=255)
@@ -21,20 +12,11 @@ class Experience(models.Model):
     description = models.TextField()
     tags = models.CharField(max_length=255, help_text ="Pisahkan dengan koma")
 
-    category = models.CharField(max_length=20, choices=EXPERIENCE_CHOICES,
-    default='full-time')
-    thumbnail = models.URLField(blank=True, null=True)
-    started_at = models.DateTimeField(auto_now_add=True)
-    ended_at = models.DateTimeField(blank=True, null=True)
-
     class Meta:
         ordering = ["order"]
 
     def __str__(self):
         return self.title
-    def tag_list(self):
-        return [tag.strip() for tag in self.tags.split]
-    @property
-    def is_ongoing(self):
-        return self.ended_at is None
     
+    def tag_list(self):
+        return [tag.strip() for tag in self.tags.split(",")]
